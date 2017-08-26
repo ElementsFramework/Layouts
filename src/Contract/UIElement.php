39ce874c0ae0189c3a523f592494c100365c31f5
Igor Rinkovec <shiqqqi@gmail.com>
2017-08-26 15:44:31 +0200

    Init.

---
+++ src/Contract/UIElement.php
@@ -0,0 +1,170 @@
+<?php
+
+
+namespace ElementsFramework\Layout\Contract;
+
+use Illuminate\View\View;
+
+/**
+ * Class UIElement
+ * @package ElementsFramework\Layout\Contract
+ */
+abstract class UIElement
+{
+
+    /**
+     * Namespace of the package the element is published in definition.
+     * @var string
+     */
+    private $namespace;
+
+    /**
+     * User readable name of the UI element that will be shown in the builder.
+     * @var string
+     */
+    private $name;
+
+    /**
+     * HTML element that renders the icon that is shown for the element in the builder.
+     * NOTE: Please use Font awesome for any publicly available elements.
+     * @var string
+     */
+    private $icon;
+
+    /**
+     * Sets the template that will be rendered inside of this UI element.
+     * This is usually used for wrapper elements (containing other elements in them).
+     * For more details visit https://github.com/ElementsFramework/LayoutBuilderUI
+     * @var string
+     */
+    private $content = null;
+
+    /**
+     * This array contains the default content data definition that will be rendered on init.
+     * This is usually used for wrapper elements (containing other elements in them).
+     * For more details visit https://github.com/ElementsFramework/LayoutBuilderUI
+     * @var array
+     */
+    private $contentData = null;
+
+    /**
+     * Settings definition array that defines the settings form schema.
+     * For more details visit https://github.com/ElementsFramework/LayoutBuilderUI
+     * @var array
+     */
+    private $settingsDefinition = null;
+
+    /**
+     * Options that are used to render the element.
+     * @var array
+     */
+    private $options = [];
+
+    /**
+     * Data used inside the render template.
+     * @var array
+     */
+    private $data = [];
+
+    /*
+     * Methods
+     */
+
+
+    /**
+     * Allows you to pass data to the view when it gets rendered.
+     * @param View $view
+     */
+    public abstract static function renderViewComposer($app, View $view);
+
+    /**
+     * Returns the view name this element renders.
+     * @return string
+     */
+    public abstract static function getViewName();
+
+    /*
+     * Getters & Setters
+     */
+
+    /**
+     * @return string
+     */
+    public function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContentData()
+    {
+        return $this->contentData;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSettingsDefinition()
+    {
+        return $this->settingsDefinition;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+}
---
+++ src/LayoutServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ElementsFramework\Layout;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LayoutServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstraps the package.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Migrations
+        $this->loadMigrationsFrom(__DIR__ . '/Migration');
+
+        // Configuration publishing
+        $this->publishes([
+            __DIR__.'/Configuration/layout.php' => config_path('layout.php'),
+        ]);
+        $this->mergeConfigFrom(
+            __DIR__.'/Configuration/layout.php', 'layout'
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Commands
+        /*$this->commands([
+            Console\CompileRoutesCommand::class,
+            Console\PublishProvidedRoutesCommand::class,
+            Console\CleanupProvidedRoutesCommand::class,
+            Console\SyncProvidedRoutesCommand::class,
+        ]);*/
+
+        // Services
+        /*$this->app->bind('ElementsFramework\DynamicRouting\Service\Compiler\RouteDeclarationCompiler', function($app) {
+            return new Service\Compiler\RouteDeclarationCompiler();
+        });*/
+    }
+
+}
